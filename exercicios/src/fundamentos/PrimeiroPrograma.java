55f394c03d860d5f33612e90612f765e41dd6fa9
Fabio Ramos Alves <fralves@viajanetsp.local>
2022-09-06 13:52:45 -0300

    incluindo arquivos java

--- exercicios/src/fundamentos/PrimeiroPrograma.java
+++ exercicios/src/fundamentos/PrimeiroPrograma.java
@@ -1,10 +1,9 @@
 package fundamentos;
 
 public class PrimeiroPrograma {
-	
-   public static void main(String[] args) {
 
-	   System.out.println("Hello world");
-}
+	public static void main(String[] args) {
 
+		System.out.println("Hello World");
+	}
 }
