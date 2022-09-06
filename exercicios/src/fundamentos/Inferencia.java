49b6677311b9a88861c1454c9f98aea702581471
Fabio Ramos Alves <fralves@viajanetsp.local>
2022-09-06 19:08:35 -0300

    tipos primitivos

--- exercicios/src/fundamentos/Inferencia.java
+++ exercicios/src/fundamentos/Inferencia.java
@@ -4,17 +4,17 @@ public class Inferencia {
 
 	public static void main(String[] args) {
 
-		//inferencia utilizado var 
+		// inferencia utilizado var
 		double a = 4.5;
 
 		var b = 4.5;
 
 		var c = "Texto";
-		
-		double d;//declaração
-		
-		d = 123.65;//inicialização
 
-		System.out.println(a + " " + b + " " + c + " " + d); //utilização
+		double d;// declaração
+
+		d = 123.65;// inicialização
+
+		System.out.println(a + " " + b + " " + c + " " + d); // utilização
 	}
 }
---
+++ exercicios/src/fundamentos/TiposPrimitivos.java
@@ -0,0 +1,37 @@
+package fundamentos;
+
+public class TiposPrimitivos {
+
+	public static void main(String[] args) {
+
+		// informação do funcionario
+
+		byte anosDeEmpresa = 23;
+		short numeroDeVoos = 542;
+		int id = 56789;
+		long pontosAcumulados = 5_234_845_223L; //L maiúsculo para informar que long
+			
+		//tipos numérico com ponto flutuante
+		
+		float salario = 11_445.44F; //literal float
+		
+		double vendasAcumuladas = 2_991_737_103.01;		
+		
+		boolean estaDeFerias = false;
+		
+		estaDeFerias = true;
+		
+		char status = 'S';
+		
+		char unicode = '\u2661';	
+		
+		System.out.println(anosDeEmpresa * 365);
+		System.out.println(numeroDeVoos / 2 );
+		System.out.println(pontosAcumulados / vendasAcumuladas);
+		System.out.println(id + ": ganha ->" + salario);
+		System.out.println("Férias: " + estaDeFerias);
+		System.out.println("Status " + status);
+		System.out.println("unicode " + unicode );
+		
+	}
+}
