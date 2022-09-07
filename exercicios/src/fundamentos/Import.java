b45d2083e09d66a7d09020b8da3c993da2a469e0
Fabio Ramos Alves <fralves@viajanetsp.local>
2022-09-07 12:19:44 -0300

    import no java

---
+++ exercicios/src/fundamentos/Import.java
@@ -0,0 +1,18 @@
+package fundamentos;
+
+import java.util.Date;
+
+public class Import {
+
+	public static void main(String[] args) {
+
+		java.lang.String s = "bom dia"; // java.lang. import já está presente
+
+		System.out.println(s);
+
+		Date date = new Date();
+
+		System.out.println(date);
+
+	}
+}
