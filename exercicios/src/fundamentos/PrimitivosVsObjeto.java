32bfc835a159b2341870efac8204a911afab826d
Fabio Ramos Alves <fralves@viajanetsp.local>
2022-09-07 13:32:51 -0300

    Tipos primitivos vs ojetos

--- exercicios/src/fundamentos/PrimitivosVsObjeto.java
+++ exercicios/src/fundamentos/PrimitivosVsObjeto.java
@@ -1,5 +1,16 @@
 package fundamentos;
 
 public class PrimitivosVsObjeto {
+	public static void main(String[] args) {
 
+		String s = "texto";
+
+		s = s.toUpperCase();
+
+		int a = 123;
+		
+		//Wrappers
+		
+		System.out.println(a);
+	}
 }
