92300d581095c8d98f34b383fc1e77f5ee8617d7
Fabio Ramos Alves <fralves@viajanetsp.local>
2022-09-08 20:21:15 -0300

    DesafioOperadoresAritmeticos

---
+++ exercicios/src/fundamentos/DesafioAritmetico.java
@@ -0,0 +1,17 @@
+package fundamentos;
+
+public class DesafioAritmetico {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a = 3 * 4 - 10;
+		
+		int b = (int)Math.pow(a, 3);
+		
+		double c = (int)Math.pow(a, 3);
+		
+		System.out.println(b + "\n" + c);
+
+	}
+}
