c4aa67414272abc61e6faaf1cef23fb1ecbf4a37
Fabio Ramos Alves <fralves@viajanetsp.local>
2022-09-10 18:58:34 -0300

    doWhile

--- exercicios/src/EstruturaDeControle/DoWhile.java
+++ exercicios/src/EstruturaDeControle/DoWhile.java
@@ -1,9 +1,29 @@
 package EstruturaDeControle;
 
+import java.util.Scanner;
+
 public class DoWhile {
 
 	public static void main(String[] args) {
+
+		// if(....) sentença; ou {}
+		// While(...) sentença; ou {}
+		// for(...; ...; ...) sentença; ou {}
+
+		// do sentença; {} while(...);
+
+		Scanner scanner = new Scanner(System.in);
 		
-		
+		String texto = "";
+
+		do {
+			System.out.println("Vc precisa falar as palavras mágicas...");
+			System.out.println("Quer Sair? ");
+			
+			texto = scanner.nextLine();
+
+		} while (!texto.equalsIgnoreCase("por favor"));
+
+		scanner.close();
 	}
 }
