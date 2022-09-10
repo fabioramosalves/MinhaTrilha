74dd088c5ca1690dce0069590b8293e0b7b28373
Fabio Ramos Alves <fralves@viajanetsp.local>
2022-09-10 15:47:27 -0300

    inclusão de exercicio temperatura

--- exercicios/src/fundamentos/exerciciosModulo/CelsiusFahrenheit.java
+++ exercicios/src/fundamentos/exerciciosModulo/CelsiusFahrenheit.java
@@ -1,7 +1,25 @@
 package fundamentos.exerciciosModulo;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class CelsiusFahrenheit {
 	public static void main(String[] args) {
 
+		double f, c;
+
+		Locale.setDefault(Locale.US);
+
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("Digite um número");
+
+		c = scanner.nextDouble();
+
+		f = c * 1.8 + 32;
+
+		System.out.printf("Fahrenheit %.2f", f);
+
+		scanner.close();
 	}
 }
--- exercicios/src/fundamentos/exerciciosModulo/IMC.java
+++ exercicios/src/fundamentos/exerciciosModulo/IMC.java
@@ -2,6 +2,8 @@ package fundamentos.exerciciosModulo;
 
 public class IMC {
 	public static void main(String[] args) {
+		
+		
 
 	}
 }
