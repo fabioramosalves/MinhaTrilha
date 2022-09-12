8c19da1d482fcd3fe7b5f67e6b90056c6e780524
Fabio Ramos Alves <fralves@viajanetsp.local>
2022-09-12 18:23:07 -0300

    desafio for

---
+++ exercicios/src/estruturaDeControle/DesafioFor.java
@@ -0,0 +1,14 @@
+package estruturaDeControle;
+
+public class DesafioFor {
+	public static void main(String[] args) {
+
+		String valor = "#";
+		for(valor.equals("#"); !valor.equals("#######"); valor += "#"){
+			System.out.println(valor);
+		}
+		
+		//Versão do desafio
+		// não pode usar o valor numerico o laço
+	}
+}
