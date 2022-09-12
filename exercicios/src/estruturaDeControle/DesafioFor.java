202c9cd6bfc5662e5cb584cea9b2bcd2c6e545fb
Fabio Ramos Alves <fralves@viajanetsp.local>
2022-09-12 18:57:35 -0300

    SwitchSemBreak

--- exercicios/src/estruturaDeControle/DesafioFor.java
+++ exercicios/src/estruturaDeControle/DesafioFor.java
@@ -3,12 +3,12 @@ package estruturaDeControle;
 public class DesafioFor {
 	public static void main(String[] args) {
 
+		//Versão do desafio
+		// não pode usar o valor numerico o laço
+		
 		String valor = "#";
 		for(valor.equals("#"); !valor.equals("#######"); valor += "#"){
 			System.out.println(valor);
 		}
-		
-		//Versão do desafio
-		// não pode usar o valor numerico o laço
 	}
 }
