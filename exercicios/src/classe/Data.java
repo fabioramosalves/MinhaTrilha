66c86e0076da688cb48f33d5142e47b0b1ded9ba
Fabio Ramos Alves <fralves@viajanetsp.local>
2022-09-13 20:23:46 -0300

    construtores padrao

--- exercicios/src/classe/Data.java
+++ exercicios/src/classe/Data.java
@@ -5,11 +5,23 @@ public class Data {
 	int dia;
 	int mes;
 	int ano;
-	
+
+	Data() {
+		dia = 1;
+		mes = 1;
+		ano = 1970;
+	}
+
+	Data(int dia, int mes, int ano) {
+		this.dia = dia;
+		this.mes = mes;
+		this.ano = ano;
+	}
+
 	public String obterDataFormatada() {
 		return String.format("%s/%s/%d", dia, mes, ano);
 	}
-	
+
 	void imprimirDataFormatada() {
 		System.out.println(obterDataFormatada());
 	}
