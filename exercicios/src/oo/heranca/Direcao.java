4717d02fe5735138ba329e9fc50d3ecafd076d2a
Fabio Ramos Alves <fralves@viajanetsp.local>
2022-09-21 09:30:29 -0300

    desafio herança

---
+++ exercicios/src/oo/heranca/Direcao.java
@@ -0,0 +1,8 @@
+package oo.heranca;
+
+public enum Direcao {
+	Norte,
+	Leste,
+	Sul,
+	Oeste;
+}
---
+++ exercicios/src/oo/heranca/desafio/Ferrari.java
@@ -0,0 +1,10 @@
+package oo.heranca.desafio;
+
+public class Ferrari extends Carro {
+
+	void acelerar() {
+		super.acelerar();
+		super.acelerar();
+		super.acelerar();
+	}
+}
