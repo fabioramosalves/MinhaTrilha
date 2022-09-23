4815d5e93f25d720ddbc66b00152cb035cd35606
Fabio Ramos Alves <fralves@viajanetsp.local>
2022-09-23 19:36:40 -0300

    outros

--- exercicios/src/streamAPI/Aluno.java
+++ exercicios/src/streamAPI/Aluno.java
@@ -1,5 +1,7 @@
 package streamAPI;
 
+import java.util.Objects;
+
 public class Aluno {
 
 	final String nome;
@@ -16,5 +18,26 @@ public class Aluno {
 
 	public double getNota() {
 		return nota;
+	}
+	
+	public String toString() {
+		return this.nome + " -> " + this.nota;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(nome, nota);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Aluno other = (Aluno) obj;
+		return Objects.equals(nome, other.nome) && Double.doubleToLongBits(nota) == Double.doubleToLongBits(other.nota);
 	}
 }
---
+++ exercicios/src/streamAPI/Outros.java
@@ -0,0 +1,38 @@
+package streamAPI;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
+import myClass.Util;
+
+public class Outros {
+
+	public static void main(String[] args) {
+
+		Aluno a1 = new Aluno("Ana", 7.1);
+		Aluno a2 = new Aluno("Luna", 6.1);
+		Aluno a3 = new Aluno("Gui", 8.1);
+		Aluno a4 = new Aluno("Gabi", 10);
+		Aluno a5 = new Aluno("Ana", 7.1);
+		Aluno a6 = new Aluno("Luna", 6.1);
+		Aluno a7 = new Aluno("Gui", 8.1);
+		Aluno a8 = new Aluno("Gabi", 10);
+
+		List<Aluno> alunos = Arrays.asList(a1, a2, a3, a4, a5, a6, a7, a8);
+
+		// para funcionar o distinct deve ser implementada a função
+		// equals e hashCode na classe Aluno
+		System.out.println("distinct...");
+		alunos.stream().distinct().forEach(System.out::println);
+
+		System.out.println("\nSkip...");
+		alunos.stream().skip(5).forEach(System.out::println);
+		System.out.println("\nLimit...");
+		alunos.stream().limit(2).forEach(System.out::println);
+
+		System.out.println("\ntakeWhile...");
+		alunos.stream().takeWhile(c -> c.nota >= 7).forEach(System.out::println);
+
+	}
+}
