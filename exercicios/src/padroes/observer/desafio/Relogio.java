d488d973f65f21107b3cbc85feacaf9446aa69ec
Fabio Ramos Alves <fralves@viajanetsp.local>
2022-09-30 15:00:25 -0300

    removendo imports

--- exercicios/src/padroes/observer/desafio/Relogio.java
+++ exercicios/src/padroes/observer/desafio/Relogio.java
@@ -1,18 +1,11 @@
 package padroes.observer.desafio;
-
-import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
 
 public class Relogio {
 
 	private List<ObservadorEventoDespertar> observadores = new ArrayList<>();
-
-	private static Map<Integer, DiaSemana> diaSemana = new HashMap<>();
 
 	Relogio() {
 		
