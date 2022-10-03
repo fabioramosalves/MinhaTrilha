113491933536e4daf01f6e00ef597ad6b0d8f974
Fabio Ramos Alves <fralves@viajanetsp.local>
2022-10-03 18:01:16 -0300

    incluindo table estados

---
+++ banco-relacional/CriarTabelaEstado.sql
@@ -0,0 +1,12 @@
+use wm;
+
+create table estados (
+    id int UNSIGNED not null AUTO_INCREMENT,
+    nome varchar(45) not null,
+    sigla varchar(2) not null,
+    regiao enum('Norte', 'Nordeste', 'Centro-Oeste', 'Sudeste', 'Sul') not null,
+    populacao decimal(5,2) not null,
+    primary key (id),
+    unique key (nome),
+    unique key (sigla)
+);
